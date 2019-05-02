67a29691d915341228b3a127420df9170b1f22f0
Kris8585 <47618122+Kris8585@users.noreply.github.com>
2019-05-01 19:10:18 -0600

    Se agregan los componentes principales al proyecto

--- sce-web/src/app/app.module.ts
+++ sce-web/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { StudentsListComponent } from './components/students-list/students-list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    StudentsListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
